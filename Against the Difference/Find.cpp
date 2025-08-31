550e1263a45f2c1f9a38960202a6066f0a2d4f07
ayman <ayman.tounsi12@gmail.com>
2025-08-31 18:26:50 +0100

    Find the frequency of each element & create Find.cpp for testing

---
+++ Against the Difference/Find.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        unordered_map<int,int> freq;
+
+        for ( auto i : a )
+            cout << freq[i]++ << endl;
+        for ( auto i : freq) 
+            cout << i.first << " : " << i.second << endl;
+    
+
+    }
+}
